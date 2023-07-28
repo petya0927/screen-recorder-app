56b3c43c08ce475b33ee752419fba021287ddce5
petya0927 <unyipeti@gmail.com>
2023-07-28 15:13:47 +0200

    disabled devtools autoopen

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Enable remote module
   require("@electron/remote/main").enable(mainWindow.webContents);
